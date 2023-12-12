ea96328c5e0ca47e5e9d9ec230b50cf6bb926743
Abdel Salas <abdelasm8@gmail.com>
2023-12-12 00:37:01 -0400

    11dic

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-const buttonEdit = document.querySelector('.edit__button')
-const popupProfile = document.querySelector('.popup_content_edit-profile')
-const closeButton = popupProfile.querySelector('.popup__button-close')
+const buttonEdit = document.querySelector('.edit__button');
+const popupProfile = document.querySelector('.popup_content_edit-profile');
+const closeButton = popupProfile.querySelector('.popup__button-close');
 
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
-const formProfile =popupProfile.querySelector('.popup__form')
+const formProfile = popupProfile.querySelector('.popup__form');
 const inputName = popupProfile.querySelector('.form__input[name="name"]');
 const inputJob = popupProfile.querySelector('.form__input[name="job"]');
 
-function togglePopup(popup){
-popupProfile.classList.toggle('popup_show')
+function togglePopup(popup) {
+  popupProfile.classList.toggle('popup_show');
 }
 
-buttonEdit.addEventListener('click', function(){
-togglePopup(popupProfile);
+buttonEdit.addEventListener('click', function() {
+  togglePopup(popupProfile);
 });
 
-closeButton.addEventListener('click', function(){
-togglePopup(popupProfile);
+closeButton.addEventListener('click', function() {
+  togglePopup(popupProfile);
 });
 
-formProfile.addEventListener('submit', function(event){
-event.preventDefault();
-profileName.textContent = inputName.value;
-profileJob.textContent = inputJob.value;
-formProfile.reset();
-togglePopup(popupProfile);
-})
+formProfile.addEventListener('submit', function(event) {
+  event.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  formProfile.reset();
+  togglePopup(popupProfile);
+});
