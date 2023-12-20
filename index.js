05043e826bb157563462b1dca6078572e7fe6f73
Abdel Salas <abdelasm8@gmail.com>
2023-12-19 23:10:39 -0400

    19dic

--- index.js
+++ index.js
@@ -1,29 +1,136 @@
-const buttonEdit = document.querySelector('.edit__button');
-const popupProfile = document.querySelector('.popup_content_edit-profile');
-const closeButton = popupProfile.querySelector('.popup__button-close');
+const buttonEdit = document.querySelector(".button_edit");
+const popupProfile = document.querySelector(".popup_edit");
 
-const profileName = document.querySelector('.profile__name');
-const profileJob = document.querySelector('.profile__job');
-const formProfile = popupProfile.querySelector('.popup__form');
-const inputName = popupProfile.querySelector('.form__input[name="name"]');
-const inputJob = popupProfile.querySelector('.form__input[name="job"]');
+const buttonAdd = document.querySelector(".button_add");
+const popupAdd = document.querySelector(".popup_add");
 
-function togglePopup(popup) {
-  popupProfile.classList.toggle('popup_show');
+const buttonClose = popupProfile.querySelector(".button_close");
+const buttonCloseAdd = popupAdd.querySelector(".button_close");
+
+const profileName = document.querySelector(".profile__name");
+const profileJob = document.querySelector(".profile__job");
+const profileForm = document.querySelector(".popup__form");
+const inputName = document.querySelector(".popup__input-name");
+const inputJob = document.querySelector(".popup__input-job");
+const currentName = profileName.textContent;
+const currentJob = profileJob.textContent;
+
+const addTitle = document.querySelector(".card__title");
+const addImage = document.querySelector(".card__image");
+const addForm = document.querySelector(".popup__form_add");
+const inputTitle = document.querySelector(".popup__input-title");
+const inputImage = document.querySelector(".popup__input-image");
+const popupImage = document.querySelector(".popup_image");
+const buttonCloseImage = popupImage.querySelector(".button_close");
+const popupImageTitle = document.querySelector(".popup__title_img");
+
+
+const initialCards = [
+  {
+    name: "valle yosemite",
+    link: "./images/kirill-pershin-1088404-unsplash.jpg",
+  },
+  {
+    name: "Tucacas",
+    link: "./images/lalo-hernandez-jCYpHYIgXE4-unsplash.jpg",
+  },
+  {
+    name: "Lago luise",
+    link: "./images/kirill-pershin-1404681-unsplash.png",
+  },
+  {
+    name: "colorado",
+    link: "./images/tim-mossholder-K1M_lwZXJWc-unsplash.jpg",
+  },
+  {
+    name: "Montañas calvas",
+    link: "./images/kirill-pershin-1556355-unsplash.png",
+  },
+  {
+    name: "Gran sabana",
+    link: "./images/jorge-salvador-Dmo1Zbi_WsU-unsplash.jpg",
+  },
+];
+
+
+const container = document.querySelector(".cards");
+
+function createCard(title, link) {
+  const template = document.querySelector("#cards-template").content;
+  const card = template.querySelector(".card").cloneNode(true);
+
+  const cardImage = card.querySelector(".card__image");
+  const cardTitle = card.querySelector(".card__title");
+  const popupImageElement = document.querySelector(".popup__element");
+
+  const buttonLike = card.querySelector(".button_like");
+  buttonLike.addEventListener("click", function () {
+    buttonLike.classList.toggle("liked");
+  });
+
+  const buttonDelete = card.querySelector(".button_delete");
+  buttonDelete.addEventListener("click", function () {
+    const card = buttonDelete.closest(".card");
+    card.remove();
+  });
+
+  cardTitle.innerText = title;
+  cardImage.src = link;
+
+  container.prepend(card);
+
+  const buttonImage = card.querySelector(".card__image");
+  buttonImage.addEventListener("click", function () {
+    popupImageElement.src = cardImage.src;
+    togglePopup(popupImage);
+    popupImageTitle.textContent = cardTitle.textContent;
+  });
 }
 
-buttonEdit.addEventListener('click', function() {
-  togglePopup(popupProfile);
+initialCards.forEach(function (cardData) {
+  createCard(cardData.name, cardData.link);
 });
 
-closeButton.addEventListener('click', function() {
+
+inputName.value = currentName;
+inputJob.value = currentJob;
+
+function togglePopup(popup) {
+  popup.classList.toggle("popup_show");
+}
+
+buttonEdit.addEventListener("click", function () {
   togglePopup(popupProfile);
 });
-
-formProfile.addEventListener('submit', function(event) {
+buttonClose.addEventListener("click", function () {
+  togglePopup(popupProfile);
+});
+profileForm.addEventListener("submit", function (event) {
   event.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
-  formProfile.reset();
+  profileForm.reset();
   togglePopup(popupProfile);
+});
+
+
+buttonAdd.addEventListener("click", function () {
+  togglePopup(popupAdd);
+});
+
+buttonCloseAdd.addEventListener("click", function () {
+  togglePopup(popupAdd);
+});
+
+addForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  createCard(inputTitle.value, inputImage.value);
+
+  addForm.reset();
+  togglePopup(popupAdd);
+});
+
+buttonCloseImage.addEventListener("click", function () {
+  togglePopup(popupImage);
 });
